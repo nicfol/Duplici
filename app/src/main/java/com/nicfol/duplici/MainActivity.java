6eaf20941cac0e5820400e4c8895e001024b1ed1
Nicolai <nfoldager0@gmail.com>
2017-07-30 16:02:53 +0200

    Added edit paste, however it's ugly and spaghetti

--- app/src/main/java/com/nicfol/duplici/MainActivity.java
+++ app/src/main/java/com/nicfol/duplici/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.facebook.stetho.Stetho;
@@ -110,4 +111,5 @@ public class MainActivity extends AppCompatActivity {
     public void updatePasteList() {
         pasteList = pasteListSingleton.getPasteList();
     }
+
 }
--- app/src/main/java/com/nicfol/duplici/RVAdapter.java
+++ app/src/main/java/com/nicfol/duplici/RVAdapter.java
@@ -2,9 +2,13 @@ package com.nicfol.duplici;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,21 +20,37 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
     private Context mContext;
     private List<Paste> pasteList;
 
+    private boolean isEditModeActive;
+
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public View normalLayout;
+        public View editLayout;
+
+        //Normal Layout views
         public TextView cLabel, cText;
         public ImageView cIcon;
+        public ImageButton editButton;
+
+        //Edit Layout Views
+        public Button dismissBtnEdit;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
+            normalLayout = (View) itemView.findViewById(R.id.normalMode);
+            editLayout = (View) itemView.findViewById(R.id.editMode);
+            isEditModeActive = false;
+
+            //Normal layout
             cLabel = (TextView) itemView.findViewById(R.id.pasteLabel);
-
-
             cText = (TextView) itemView.findViewById(R.id.pasteText);
-            //if(cText.length() > 40) { cText.setTextSize(17); } //TODO Scale text based on length?
-
-
             cIcon = (ImageView) itemView.findViewById(R.id.pasteIcon);
+
+            editButton = (ImageButton) itemView.findViewById(R.id.editButton);
+
+            //Editing Layout
+            dismissBtnEdit = (Button) itemView.findViewById(R.id.dismissBtnEdit);
+
         }
     }
 
@@ -47,11 +67,39 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(RVAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RVAdapter.ViewHolder holder, int position) {
         holder.setIsRecyclable(false);
-        holder.cLabel.setText(pasteList.get(position).getDbID() + " " + pasteList.get(position).getLabel());
-        holder.cText.setText(position + pasteList.get(position).getText());
-        holder.cIcon.setImageResource(R.drawable.moneybag); //TODO Assign DB value to imageview
+        holder.cLabel.setText(pasteList.get(position).getLabel());
+        holder.cText.setText(pasteList.get(position).getText());
+        //holder.cIcon.setImageResource(R.drawable.moneybag); //TODO Assign DB value to imageview
+
+
+
+        //Event Listener for editing the layout
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeLayoutToEditing(holder);
+            }
+        });
+        //Event Listener for editing the layout
+        holder.dismissBtnEdit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeLayoutToEditing(holder);
+                Log.d("Dismiss Button: ", "Has been pressed");
+            }
+        });
+    }
+
+    public void changeLayoutToEditing(RVAdapter.ViewHolder holder) {
+        if(!isEditModeActive) {
+            holder.normalLayout.setVisibility(View.GONE);
+            holder.editLayout.setVisibility(View.VISIBLE);
+            isEditModeActive = true;
+        } else {
+            holder.normalLayout.setVisibility(View.VISIBLE);
+            holder.editLayout.setVisibility(View.GONE);
+            isEditModeActive = false;
+        }
     }
 
     @Override
@@ -63,4 +111,5 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
     public int getItemCount() {
         return pasteList.size();
     }
+
 }
